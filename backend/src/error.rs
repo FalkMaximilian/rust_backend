888f333b031dd4d8bee9f84add056f0948547065
Maximilian Falk <falk.maximilian@outlook.com>
2024-02-04 01:06:53 +0100

    logging

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -1,8 +1,10 @@
 use axum::{http::StatusCode, response::IntoResponse};
+use serde::Serialize;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
 
@@ -19,7 +21,13 @@ impl IntoResponse for Error {
     fn into_response(self) -> axum::response::Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create placeholder axum response
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the Response
+        response.extensions_mut().insert(self);
+
+        response
     }
 }
 
@@ -30,3 +38,36 @@ impl std::fmt::Display for Error {
 }
 
 impl std::error::Error for Error {}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // -- Auth
+            Self::AuthFailTokenWrongFormat
+            | Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // -- Model
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // -- Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
+}
